cd4081b350df05a8c4e145f07a0e67a1f976c9db
adsilcott <adsilcott@gmail.com>
2025-04-03 15:42:31 -0400

    Fixed frontmatter not updating

--- main.ts
+++ main.ts
@@ -1,92 +1,51 @@
-import { App, Editor, editorEditorField, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, editorEditorField, MarkdownView, MetadataCache, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, WorkspaceLeaf } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface WordCountPropertySettings {
+	propertyName: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: WordCountPropertySettings = {
+	propertyName: 'word_count'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class WordCountPropertyPlugin extends Plugin {
+	settings: WordCountPropertySettings;
+
+	private isRemoving = false;
+	private activeLeaf: any;
 
 	async onload() {
 		await this.loadSettings();
 
-		this.registerEvent(this.app.vault.on('modify', async () => {
-			const file = this.app.workspace.getActiveFile();
-			if (!file) return;
-			let text = await this.app.vault.read(file);
-			this.app.fileManager.processFrontMatter(file, async frontmatter => {
-				if ('word_count' in frontmatter) {
-					frontmatter['word_count'] = text.replace(/^---.*?\n---\n/s, "").split(/\b\w+\b/g).length - 1;
+		this.app.workspace.onLayoutReady(() => {
+
+			this.registerEvent(this.app.workspace.on('active-leaf-change', async (leaf) => {
+				const file = this.app.workspace.getActiveFile();
+				if (!file) return;
+				let fm = this.app.metadataCache.getFileCache(file)?.frontmatter;
+				if (fm && this.settings.propertyName in fm)
+				{
+					let text = await this.app.vault.read(file);
+					let count = text.replace(/---[\S\s]*?---/, "").split(/\b\w+\b/g).length - 1; ///^---.*?\n---\n/s
+					this.app.fileManager.processFrontMatter(file, async (frontmatter) => {frontmatter[this.settings.propertyName] = count});
 				}
-			})
-		}))
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			}))
+	
+			// This adds an editor command that can perform some operation on the current editor instance
+			this.addCommand({
+				id: 'remove-tracked-property',
+				name: 'Remove Tracked Property',
+				editorCallback: (editor: Editor, view: MarkdownView) => {
+					this.isRemoving = true;
+					const file = this.app.workspace.getActiveFile();
+					if (!file) return;
+					this.app.fileManager.processFrontMatter(file, frontmatter => {delete frontmatter[this.settings.propertyName]});
 				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			});
+			
+			// This adds a settings tab so the user can configure various aspects of the plugin
+			this.addSettingTab(new SettingTab(this.app, this));
+		})
 	}
 
 	onunload() {
@@ -102,26 +61,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: WordCountPropertyPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: WordCountPropertyPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -132,13 +75,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Property Name')
+			.setDesc('Only notes with this property already added will be updated')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('word_count')
+				.setValue(this.plugin.settings.propertyName)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.propertyName = value;
 					await this.plugin.saveSettings();
 				}));
 	}
